1a1d25dae704785ea6f0befb29d6e19a4b6d050b
Atul Kumar Pandey akp1081@gmail.com <70027847+Akp-108@users.noreply.github.com>
2023-05-22 19:47:57 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "jenkins-pipeline-bucket1"
-    key    = "statefile/terraform.tfstatefile"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "jenkins-pipeline-bucket1"
+#     key    = "statefile/terraform.tfstatefile"
+#     region = "us-east-1"
+#   }
+# }
